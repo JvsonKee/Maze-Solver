014e810b89ad61c80a42d635f31c44592a61bf88
MatthiasKee <matthias.kee@ucalgary.ca>
2023-06-25 13:56:56 -0600

    testing package name change

---
+++ src/randomness/UnderflowException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class UnderflowException extends RuntimeException {
+    public UnderflowException() {
+       
+    }
+    public UnderflowException(String message) {
+        super(message);
+    }
+}
